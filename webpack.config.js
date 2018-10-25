c17966658cb8dbc941f171705b3539317bf799b6
NickLatkovich <nicklatkovich@gmail.com>
2018-10-25 12:28:53 +0300

    Base simple-react app

---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+require("babel-polyfill");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+require('webpack');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: `${__dirname}/src/assets/index.html`,
+	filename: 'index.html',
+	inject: 'body',
+});
+
+const extractSass = new ExtractTextPlugin({
+	filename: '[name].[hash].css',
+	disable: process.env.NODE_ENV === 'local',
+});
+
+const timeCache = Date.now();
+
+module.exports = {
+	entry: ['babel-polyfill', './src/index.jsx'],
+	output: {
+		publicPath: '/',
+		path: path.resolve('dist'),
+		filename: `[name].${timeCache}.js`,
+		pathinfo: process.env.NODE_ENV === 'local',
+		sourceMapFilename: '[name].js.map',
+		chunkFilename: `[name].bundle.js?v=${timeCache}`,
+	},
+	devtool: process.env.NODE_ENV !== 'local' ? 'cheap-module-source-map' : 'eval',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.jsx$/,
+				include: /src/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+					use: [{
+						loader: 'css-loader',
+					}, {
+						loader: 'sass-loader',
+					}],
+					// use style-loader in development
+					fallback: 'style-loader',
+				}),
+			},
+			{
+				test: /\.css$/,
+				use: extractSass.extract({
+					use: [
+						{
+							loader: 'css-loader',
+						},
+					],
+					// use style-loader in development
+					fallback: 'style-loader',
+				}),
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|otf|svg)$/,
+				loader: 'url-loader?limit=100000',
+			},
+		],
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				default: false,
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	resolve: {
+		modules: [
+			'node_modules',
+			path.resolve('src'),
+		],
+		extensions: ['.js', '.jsx', '.json'],
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		HTMLWebpackPluginConfig,
+		extractSass,
+	],
+	node: {
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty',
+	},
+};
