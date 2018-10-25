ab50ea09e9233b9e6675662bdb42c1670409d180
NickLatkovich <nicklatkovich@gmail.com>
2018-10-25 12:40:23 +0300

    Connection page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import Connection from './Connection';
+
 export default class App extends Component {
 
+	constructor(...args) {
+		super(...args);
+		this.state = { connectedToEcho: false };
+	}
+
 	render() {
+		if (!this.state.connectedToEcho) return <Connection />
 		return <div>qwe</div>;
 	}
 
---
+++ src/components/Connection.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Connection extends Component {
+
+	render() {
+		return <div id="connection">Подключение к блокчейну Echo...</div>
+	}
+	
+}
